615d4d3fa75bfa28814ef0bdeb495cd1b493b045
LithanianKanon <90057164+LithanianKanon@users.noreply.github.com>
2021-09-03 21:24:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+  function RickrollEE() {
+window.location= "Rickroll.html";
+
+  }
+
+  function back() {
+    window.location= "index.html";
+  }
+
+  function NotRickrollEE() {
+    window.location= "WrongButton.html";
+  }
+
+  function NotRickrollEE2() {
+    window.location= "WrongButton2.html";
+  }
